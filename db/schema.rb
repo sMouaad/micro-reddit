93ab564715f258d8e40668099c179a8ca1318ccf
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-09-28 13:39:52 +0100

    feat: add foreign key user_id in comment and migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_28_120005) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_28_123939) do
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "post_id", null: false
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -36,5 +38,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_28_120005) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
