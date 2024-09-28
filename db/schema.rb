1787455eda8534721e867a673e7330d733acac0b
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-09-28 13:00:49 +0100

    feat: migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_28_103417) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_28_120005) do
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -22,6 +24,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_28_103417) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +34,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_28_103417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
 end
