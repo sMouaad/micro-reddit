79c9feaa2d383f35f6c24cd44befc2ca36acf3fa
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-09-28 12:06:03 +0100

    feat: add validations to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
+
+  validates :title, :content, presence: true
+  validates :title, length: { minimum: 5 }
+  validates :content, length: { in: 10..200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments, through: :posts
+
+  validates :name, :password, presence: true
+  validates :name, length: { minimum: 2 }, uniqueness: true
+  validates :password, length: { in: 8..20 }
 end
